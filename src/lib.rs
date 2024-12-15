0807d30d35384684d2dc0d5600663ed7f736e532
Enea Giger <enea.giger@tuta.io>
2024-12-15 12:38:22 +0100

    feat: First processor doing nothing

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,23 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+pub struct Processor {}
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, audio-speed-changer!");
+impl Processor {
+    #[wasm_bindgen]
+    pub fn new(volume: f32) -> Self {
+        Self {}
+    }
+
+    #[wasm_bindgen]
+    pub fn process(&self, input_list: &mut [f32], output_list: &mut [f32]) -> bool {
+        let mut i = 0;
+        while i < input_list.len() {
+            output_list[i] = input_list[i];
+            i += 1;
+        }
+
+        return true;
+    }
 }
